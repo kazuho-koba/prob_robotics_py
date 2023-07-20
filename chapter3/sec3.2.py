67a1a7fd0a0decb7133c8fd51c37ffc0584240a0
kazuho home desktop ubuntu22 <kazuho.kobayashi@gmail.com>
2023-07-20 21:27:50 +0900

    worked on chapter 3

--- chapter3/sec3.2.py
+++ chapter3/sec3.2.py
@@ -36,24 +36,27 @@ class World:
 
         elems = []
 
+        # デバッグモードの場合
         if self.debug:
             for i in range(1000):
                 self.one_step(i, elems, ax)  # stop animation when debugging
+        # そうでない通常モードの場合
         else:
+            # アニメーションの描画
             self.ani = anm.FuncAnimation(fig, self.one_step, fargs=(
                 elems, ax), frames=int(self.time_span/self.time_interval)+1, interval=int(self.time_interval*1000), repeat=False)
             plt.show()
 
-        # draw objects which appended
-        # for obj in self.objects:
-        #     obj.draw(ax)
-        # plt.show()
-
+    # 世界の時刻を1タイムステップ進める関数
     def one_step(self, i, elems, ax):
+        # 二重描画を防ぐため1タイムステップ前の描画をすべてクリアする
         while elems:
-            elems.pop().remove()
-        time_str = "t = %.2f[s]" % (self.time_interval*i)
+            elems.pop().remove()    # elemsの要素を削除
+
+        # 時刻を表示する
         elems.append(ax.text(-4.5, 4.5, "t= "+str(i), fontsize=10))
+
+        # オブジェクトそれぞれについて描画を行う
         for obj in self.objects:
             obj.draw(ax, elems)
             if hasattr(obj, "one_step"):
@@ -63,34 +66,42 @@ class World:
 
 class IdealRobot:
     def __init__(self, pose, agent=None, color="black"):
-        self.pose = pose  # set default posture by the argument
-        self.r = 0.2  # robot radius in drawing
+        self.pose = pose    # set default posture by the argument
+        self.r = 0.2        # robot radius in drawing
         self.color = color  # set default drawing color by the argument
-        self.agent = agent
+        self.agent = agent  # ロボットがエージェントによって操縦されているイメージ
         self.poses = [pose]
 
+    # ロボットの描画を行うメソッド
     def draw(self, ax, elems):
         x, y, theta = self.pose
         xn = x+self.r*math.cos(theta)  # x coordinate value of robot's tip
         yn = y+self.r*math.sin(theta)  # y coordinate value of robot's tip
-        # draw line segment indicates robot's direction
+
+        # ロボットの向きを示す線分を描画しelemsに追加する（ax.plotはリストを返してくるのでリスト同士の足し算としている）
         elems += ax.plot([x, xn], [y, yn], color=self.color)
+
+        # ロボット自身を示す円を描画しelemsに追加する（こちらはobjectなのでappendで足す）
         c = patches.Circle(xy=(x, y), radius=self.r,
                            fill=False, color=self.color)  # make a circle indicats robot's body
-        # register the circle generated above to subplot
-        elems.append(ax.add_patch(c))
+        elems.append(ax.add_patch(c))   # register the circle generated above to subplot
+        
+        # ロボットの軌道を描画する
         self.poses.append(self.pose)
         elems += ax.plot([e[0] for e in self.poses], [e[1]
                          for e in self.poses], linewidth=0.5, color="black")
 
+    # ロボットの1ステップの間での状態遷移を示す関数。オブジェクトを作らなくてもこのメソッドを実行できるよう冒頭に@classmethodとつけている。
     @classmethod
     def state_transition(cls, nu, omega, time, pose):
+        # 以下、少し複雑だが対向二輪（差動二輪）ロボットの並進速度nuと旋回速度omegaによる運動モデル
         t0 = pose[2]
         if math.fabs(omega) < 1e-10:
             return pose + np.array([nu*math.cos(t0), nu*math.sin(t0), omega])*time
         else:
             return pose + np.array([nu/omega*(math.sin(t0+omega*time)-math.sin(t0)), nu/omega*(-math.cos(t0+omega*time)+math.cos(t0)), omega*time])
 
+    # ロボットに搭載されたエージェントの意思決定にしたがって並進、旋回速度を得て、それを元に1タイムステップ分の運動を決める
     def one_step(self, time_interval):
         if not self.agent:
             return
@@ -107,10 +118,10 @@ class Agent:
         return self.nu, self.omega
 
 
-world = World(10, 1)
+world = World(20, 1)
 # world.draw()
-straight = Agent(0.2, 0.0)  # go straight forward
-circling = Agent(0.2, 10.0/180*math.pi)  # 0.2[m/s], 10[deg/sec]
+straight = Agent(0.2, 0.0)                  # go straight forward
+circling = Agent(0.2, 10.0/180*math.pi)     # 0.2[m/s], 10[deg/sec]
 robot1 = IdealRobot(np.array([2, 3, math.pi/6]).T,
                     straight)  # make robot's instance
 robot2 = IdealRobot(np.array([-2, -1, math.pi/5*6]).T, circling, "red")
